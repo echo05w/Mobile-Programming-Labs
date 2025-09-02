1d6bdc7ec59179626bae57353822590bd4195849
Hasan Soliyev <168918473+echo05w@users.noreply.github.com>
2025-09-02 16:39:40 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,12 @@
+// Soliyev Hasan
+// 220079
 
+Tuesday 14:30
+Wednesday 9:00
+
+  Q2:
+The main function is the entry point of a C++ program .
+  Reason: It's where the program execution begins.
+In Java, the main function is static so it can be called without creating an object of the class.
+ The main() method is static so Java can call it directly when the program starts, without needing an instance of the class.
+   Q3:
